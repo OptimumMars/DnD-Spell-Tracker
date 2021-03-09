5e045a9f2c6aff2e2a0c09b4cecb4a9f34f4b152
Gabriel <gabriel.gutz@live.com>
2021-03-09 18:16:08 -0500

    added a basic character view

--- react-app/src/components/characterView.js
+++ react-app/src/components/characterView.js
@@ -18,9 +18,27 @@ const CharacterView = () => {
         fetchActiveCharacter();
     }, [characterId])
 
+    const activeCharacter = useSelector(state => state.characters.activeCharacter)
+
+    console.log(activeCharacter)
+
     return (
         <div className="parchment_paper">
             <h1>this is the character specific page</h1>
+            { activeCharacter &&
+                <div>
+                    <h3>{activeCharacter.activeCharacter.name}</h3>
+                    {activeCharacter.characterSpells && activeCharacter.characterSpells.map(spell => (
+                        <div>
+                            <h5>{spell.name}</h5>
+                            <ul>
+                                <li>{spell.level}</li>
+                                <li>{spell.description}</li>
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            }
         </div >
     )
 }
